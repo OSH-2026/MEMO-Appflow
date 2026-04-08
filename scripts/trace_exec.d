6294e6aea7b6336923aa870e0455db6eade1fd8e
javaherobrine <javaherobrine@qq.com>
2026-04-08 22:33:10 +0800

    Add some eBPF traces

---
+++ scripts/trace_exec.d
@@ -0,0 +1,16 @@
+dtrace:::BEGIN
+{
+    printf("Press Ctrl+C to halt\n");
+}
+
+/* Fires when a new process image is executed */
+proc:::exec-success
+{
+    printf("At time %Y, %s executed %s\n",
+        walltimestamp, execname, curpsinfo->pr_psargs);
+}
+
+dtrace:::END
+{
+    printf("exited\n");
+}
